ea4e82fa4b78da4dd5b30f804ad24f6a7fc0aec5
bgrrw <109300557+bgrrw@users.noreply.github.com>
2022-10-21 15:09:59 -0700

    Add files via upload

---
+++ Cities.js
@@ -0,0 +1,797 @@
+var cities = {
+  "type" : "FeatureCollection",
+  "features" : [
+    {
+      "type" : "Feature",
+      "id" : 1,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -0.17800167663602906,
+          51.487910936840308
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 1,
+        "OBJECTID" : 6,
+        "CITY_NAME" : "London",
+        "ADMIN_NAME" : "England",
+        "CNTRY_NAME" : "United Kingdom",
+        "STATUS" : "P",
+        "Long" : -0.178001676636,
+        "Lat" : 51.487910936799999,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 2,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -9.1299956023151481,
+          38.729997059616665
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 2,
+        "OBJECTID" : 7,
+        "CITY_NAME" : "Lisbon",
+        "ADMIN_NAME" : "Lisboa",
+        "CNTRY_NAME" : "Portugal",
+        "STATUS" : "National and provincial capital",
+        "Long" : -9.1299956023199993,
+        "Lat" : 38.729997059600002,
+        "test" : 39
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 3,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -3.6909716984850434,
+          40.442220073097928
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 3,
+        "OBJECTID" : 8,
+        "CITY_NAME" : "Madrid",
+        "ADMIN_NAME" : "Madrid",
+        "CNTRY_NAME" : "Spain",
+        "STATUS" : "National and provincial capital",
+        "Long" : -3.6909716984899998,
+        "Lat" : 40.442220073100003,
+        "test" : 40
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 4,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          2.1590014171816847,
+          41.357997044328158
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 4,
+        "OBJECTID" : 9,
+        "CITY_NAME" : "Barcelona",
+        "ADMIN_NAME" : "Cataluna",
+        "CNTRY_NAME" : "Spain",
+        "STATUS" : "Provincial capital",
+        "Long" : 2.1590014171799998,
+        "Lat" : 41.357997044299999,
+        "test" : 41
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 5,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          11.207745363657637,
+          43.7815709287554
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 5,
+        "OBJECTID" : 10,
+        "CITY_NAME" : "Florence",
+        "ADMIN_NAME" : "Toscana",
+        "CNTRY_NAME" : "Italy",
+        "STATUS" : "P",
+        "Long" : 11.207745363700001,
+        "Lat" : 43.781570928800001,
+        "test" : 44
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 6,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          7.4240013807591367,
+          43.745004004231475
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 6,
+        "OBJECTID" : 11,
+        "CITY_NAME" : "Monaco",
+        "ADMIN_NAME" : "Monaco",
+        "CNTRY_NAME" : "Monaco",
+        "STATUS" : "National capital",
+        "Long" : 7.42400138076,
+        "Lat" : 43.745004004199998,
+        "test" : 44
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 7,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          12.519999337999081,
+          41.879997044328206
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 7,
+        "OBJECTID" : 12,
+        "CITY_NAME" : "Rome",
+        "ADMIN_NAME" : "Lazio",
+        "CNTRY_NAME" : "Italy",
+        "STATUS" : "P",
+        "Long" : 12.519999338,
+        "Lat" : 41.879997044299998,
+        "test" : 42
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 8,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          8.6681613121948544,
+          50.130000020784394
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 8,
+        "OBJECTID" : 13,
+        "CITY_NAME" : "Frankfurt",
+        "ADMIN_NAME" : "Hessen",
+        "CNTRY_NAME" : "Germany",
+        "STATUS" : "P",
+        "Long" : 8.6681613121899996,
+        "Lat" : 50.130000020799997,
+        "test" : 50
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 9,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          2.4329973442521009,
+          48.88199706321393
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 9,
+        "OBJECTID" : 14,
+        "CITY_NAME" : "Paris",
+        "ADMIN_NAME" : "Ile-de-France",
+        "CNTRY_NAME" : "France",
+        "STATUS" : "National and provincial capital",
+        "Long" : 2.4329973442499999,
+        "Lat" : 48.881997063199996,
+        "test" : 49
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 10,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          3.2200023792065053,
+          51.209999996502688
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 10,
+        "OBJECTID" : 15,
+        "CITY_NAME" : "Bruges",
+        "ADMIN_NAME" : "West-Vlaanderen",
+        "CNTRY_NAME" : "Belgium",
+        "STATUS" : "Provincial capital",
+        "Long" : 3.2200023792099999,
+        "Lat" : 51.209999996500002,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 11,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          4.3679974391305336,
+          50.837003966459918
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 11,
+        "OBJECTID" : 16,
+        "CITY_NAME" : "Brussels",
+        "ADMIN_NAME" : "Bruxelles-Brussel",
+        "CNTRY_NAME" : "Belgium",
+        "STATUS" : "National capital and provincial capital enclave",
+        "Long" : 4.3679974391299998,
+        "Lat" : 50.837003966499999,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 12,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          4.8948393242754378,
+          52.373042982347783
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 12,
+        "OBJECTID" : 17,
+        "CITY_NAME" : "Amsterdam",
+        "ADMIN_NAME" : "Noord-Holland",
+        "CNTRY_NAME" : "Netherlands",
+        "STATUS" : "P",
+        "Long" : 4.8948393242800003,
+        "Lat" : 52.373042982299999,
+        "test" : 52
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 13,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          6.7752993151113401,
+          51.489332937664983
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 13,
+        "OBJECTID" : 18,
+        "CITY_NAME" : "Duisburg",
+        "ADMIN_NAME" : "Nordrhein-Westfalen",
+        "CNTRY_NAME" : "Germany",
+        "STATUS" : "P",
+        "Long" : 6.7752993151099998,
+        "Lat" : 51.489332937699999,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 14,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          6.7979973842719232,
+          51.240996078706132
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 14,
+        "OBJECTID" : 19,
+        "CITY_NAME" : "Dusseldorf",
+        "ADMIN_NAME" : "Nordrhein-Westfalen",
+        "CNTRY_NAME" : "Germany",
+        "STATUS" : "P",
+        "Long" : 6.7979973842700003,
+        "Lat" : 51.2409960787,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 15,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          6.9348694152913595,
+          50.94234904277414
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 15,
+        "OBJECTID" : 20,
+        "CITY_NAME" : "Cologne",
+        "ADMIN_NAME" : "Nordrhein-Westfalen",
+        "CNTRY_NAME" : "Germany",
+        "STATUS" : "P",
+        "Long" : 6.9348694152899997,
+        "Lat" : 50.942349042799997,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 16,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          6.134004296960768,
+          46.202003932735352
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 16,
+        "OBJECTID" : 21,
+        "CITY_NAME" : "Geneva",
+        "ADMIN_NAME" : "Geneve",
+        "CNTRY_NAME" : "Switzerland",
+        "STATUS" : "P",
+        "Long" : 6.1340042969599997,
+        "Lat" : 46.202003932700002,
+        "test" : 46
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 17,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          8.30300440802705,
+          47.058003076380942
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 17,
+        "OBJECTID" : 22,
+        "CITY_NAME" : "Luzern",
+        "ADMIN_NAME" : "Luzern",
+        "CNTRY_NAME" : "Switzerland",
+        "STATUS" : "P",
+        "Long" : 8.3030044080300005,
+        "Lat" : 47.058003076399999,
+        "test" : 47
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 18,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          7.4459973033329447,
+          46.947995993720212
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 18,
+        "OBJECTID" : 23,
+        "CITY_NAME" : "Bern",
+        "ADMIN_NAME" : "Bern",
+        "CNTRY_NAME" : "Switzerland",
+        "STATUS" : "P",
+        "Long" : 7.4459973033300004,
+        "Lat" : 46.947995993699998,
+        "test" : 47
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 19,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          8.5339984141954801,
+          47.401002048705664
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 19,
+        "OBJECTID" : 24,
+        "CITY_NAME" : "Zurich",
+        "ADMIN_NAME" : "Zurich",
+        "CNTRY_NAME" : "Switzerland",
+        "STATUS" : "P",
+        "Long" : 8.5339984141999992,
+        "Lat" : 47.401002048700001,
+        "test" : 47
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 20,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          14.456997316373077,
+          50.10599694720139
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 20,
+        "OBJECTID" : 25,
+        "CITY_NAME" : "Prague",
+        "ADMIN_NAME" : "Stredocesky",
+        "CNTRY_NAME" : "Czech Republic",
+        "STATUS" : "National and provincial capital",
+        "Long" : 14.456997316400001,
+        "Lat" : 50.105996947199998,
+        "test" : 50
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 21,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          16.320978443900117,
+          48.202119033866381
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 21,
+        "OBJECTID" : 26,
+        "CITY_NAME" : "Vienna",
+        "ADMIN_NAME" : "Wien",
+        "CNTRY_NAME" : "Austria",
+        "STATUS" : "National capital and provincial capital enclave",
+        "Long" : 16.3209784439,
+        "Lat" : 48.202119033899997,
+        "test" : 48
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 22,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          11.542950387550377,
+          48.140972986714019
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 22,
+        "OBJECTID" : 27,
+        "CITY_NAME" : "Munich",
+        "ADMIN_NAME" : "Bayern",
+        "CNTRY_NAME" : "Germany",
+        "STATUS" : "P",
+        "Long" : 11.542950387599999,
+        "Lat" : 48.1409729867,
+        "test" : 48
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 23,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          12.370716364168914,
+          45.424773024755325
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 23,
+        "OBJECTID" : 28,
+        "CITY_NAME" : "Venice",
+        "ADMIN_NAME" : "Veneto",
+        "CNTRY_NAME" : "Italy",
+        "STATUS" : "P",
+        "Long" : 12.3707163642,
+        "Lat" : 45.424773024799997,
+        "test" : 45
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 24,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          14.639607376479319,
+          46.068299921004609
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 24,
+        "OBJECTID" : 29,
+        "CITY_NAME" : "Ljubljana",
+        "ADMIN_NAME" : "Slovenia",
+        "CNTRY_NAME" : "Slovenia",
+        "STATUS" : "National capital",
+        "Long" : 14.639607376500001,
+        "Lat" : 46.068299920999998,
+        "test" : 46
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 25,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          15.96400238999837,
+          45.807003043555653
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 25,
+        "OBJECTID" : 30,
+        "CITY_NAME" : "Zagreb",
+        "ADMIN_NAME" : "Croatia",
+        "CNTRY_NAME" : "Croatia",
+        "STATUS" : "National capital",
+        "Long" : 15.964002389999999,
+        "Lat" : 45.807003043599998,
+        "test" : 46
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 26,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          13.327569358139442,
+          52.516268923272264
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 26,
+        "OBJECTID" : 31,
+        "CITY_NAME" : "Berlin",
+        "ADMIN_NAME" : "Berlin",
+        "CNTRY_NAME" : "Germany",
+        "STATUS" : "P",
+        "Long" : 13.3275693581,
+        "Lat" : 52.516268923299997,
+        "test" : 53
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 27,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          19.09400433563161,
+          47.514995963592924
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 27,
+        "OBJECTID" : 32,
+        "CITY_NAME" : "Budapest",
+        "ADMIN_NAME" : "Budapest",
+        "CNTRY_NAME" : "Hungary",
+        "STATUS" : "National capital and provincial capital enclave",
+        "Long" : 19.094004335600001,
+        "Lat" : 47.514995963600001,
+        "test" : 48
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 28,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -0.14357639853193405,
+          50.829162490656472
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 28,
+        "OBJECTID" : 66,
+        "CITY_NAME" : "Brighton",
+        "ADMIN_NAME" : "Brighton",
+        "CNTRY_NAME" : "United Kingdom",
+        "STATUS" : "P",
+        "Long" : 0.13720000037999999,
+        "Lat" : 50.822499999999998,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 29,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          18.094399999760242,
+          42.650700000204893
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 29,
+        "OBJECTID" : 70,
+        "CITY_NAME" : "Dubrovnik",
+        "ADMIN_NAME" : "Dubrovnik",
+        "CNTRY_NAME" : "Croatia",
+        "STATUS" : "Other",
+        "Long" : 18.0943999998,
+        "Lat" : 42.650700000199997,
+        "test" : 43
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 30,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          7.2620000002998495,
+          43.710200000030056
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 30,
+        "OBJECTID" : 79,
+        "CITY_NAME" : "Nice",
+        "ADMIN_NAME" : "Nice",
+        "CNTRY_NAME" : "France",
+        "STATUS" : "Other",
+        "Long" : 7.2620000002999996,
+        "Lat" : 43.7102,
+        "test" : 44
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 31,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          6.8637999995703467,
+          51.496299999945109
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 31,
+        "OBJECTID" : 80,
+        "CITY_NAME" : "Oberhausen",
+        "ADMIN_NAME" : "Oberhausen",
+        "CNTRY_NAME" : "Germany",
+        "STATUS" : "P",
+        "Long" : 6.8637999995700003,
+        "Lat" : 51.496299999900003,
+        "test" : 51
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 32,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          10.40170000015496,
+          43.722800000119946
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 32,
+        "OBJECTID" : 82,
+        "CITY_NAME" : "Pisa",
+        "ADMIN_NAME" : "Pisa",
+        "CNTRY_NAME" : "Italy",
+        "STATUS" : "P",
+        "Long" : 10.4017000002,
+        "Lat" : 43.722800000100001,
+        "test" : 44
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 33,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          16.440200000030018,
+          43.508099999815215
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 33,
+        "OBJECTID" : 88,
+        "CITY_NAME" : "Split",
+        "ADMIN_NAME" : "Split",
+        "CNTRY_NAME" : "Croatia",
+        "STATUS" : "Other",
+        "Long" : 16.440200000000001,
+        "Lat" : 43.508099999800002,
+        "test" : 44
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 34,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          12.453399999810188,
+          41.902899999735268
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 34,
+        "OBJECTID" : 91,
+        "CITY_NAME" : "Vatican City",
+        "ADMIN_NAME" : "Vatican City",
+        "CNTRY_NAME" : "Vatican City",
+        "STATUS" : "P",
+        "Long" : 12.4533999998,
+        "Lat" : 41.902899999699997,
+        "test" : 42
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 35,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          7.7490999997652352,
+          46.020700000304828
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 35,
+        "OBJECTID" : 94,
+        "CITY_NAME" : "Zermatt",
+        "ADMIN_NAME" : "Zermatt",
+        "CNTRY_NAME" : "Switzerland",
+        "STATUS" : "P",
+        "Long" : 7.7490999997700003,
+        "Lat" : 46.020700000300003,
+        "test" : 46
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 36,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -1.8254899999634517,
+          51.176870000250346
+        ]
+      },
+      "properties" : {
+        "OBJECTID_1" : 36,
+        "OBJECTID" : null,
+        "CITY_NAME" : "Stonehenge",
+        "ADMIN_NAME" : "England",
+        "CNTRY_NAME" : "United Kingdom",
+        "STATUS" : null,
+        "Long" : null,
+        "Lat" : null,
+        "test" : null
+      }
+    }
+  ]
+}
